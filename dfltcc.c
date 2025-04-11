andreasstieger/gzip

commit 23ddd95f3e152423fe0dfc96ff31d4130a65e8cf

gzip: better fix for s390 buffer flushes
* dfltcc.c (dfltcc_inflate): The previous patch was incorrect, as
fill_inbuf (0) will not call flush_outbuf which is needed here.
Problem and fix reported by Eduard Stefes in
<https://debbugs.gnu.org/cgi/bugreport.cgi?bug=75924#13>.
Although this patch causes the hufts test to fail, that might be
due to a different bug in the s390 code, so let’s leave that test
alone for now.

dfltcc.c
@@ -434,7 +434,13 @@ dfltcc_inflate ()
         flush_outbuf ();
       if (inptr == insize)
         {
-          fill_inbuf (0);
+          if (fill_inbuf (1) == EOF)
+            {
+              /* Premature EOF.  */
+              flush_outbuf ();
+              errno = 0;
+              read_error ();
+            }
           inptr = 0;
         }
 

commit 60f9518f7c7ae729eb38f0a07721a05b255bf007

maint: include errno.h
Found building for s390x on openSUSE

Fixes: 23ddd95 "gzip: better fix for s390 buffer flushes"

dfltcc.c
@@ -18,6 +18,7 @@
 #include <config.h>
 
 #include <stdlib.h>
+#include <errno.h>
 
 #ifdef HAVE_SYS_SDT_H
 # include <sys/sdt.h>